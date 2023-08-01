e47f320737a1b7624b3bfa25ba261bc976d4abb2
Nigel-Fink <WINLOCER1989@gmail.com>
2023-08-01 20:58:23 +0400

    Gurls now turns their heads to ya
    Update RubberWoman prefabs. Added easiest script that i probably wrote in 10mins is not count time of IRL chores i did while writing. Now rubbergurls look follows you, drilling your soul with terror.

---
+++ Assets/Content/Exhibitions-Arts/1-K-Rezine/Script/RotationTracker.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RotationTracker : UdonSharpBehaviour
+{
+    public Vector3 CenterOffset;
+    public bool TrackPlayer;
+    public Transform RotatableTransform;
+    public Vector3 AdjustmentRotation;
+    private Quaternion ZeroRotation;
+    public float MaxDetectionDistance = 1f;
+
+    void Start()
+    {
+        ZeroRotation = RotatableTransform.rotation;
+    }
+
+    public void UpdateZeroRotation()
+    {
+        ZeroRotation = RotatableTransform.localRotation * Quaternion.Inverse(Quaternion.Euler(AdjustmentRotation));
+    }
+
+    void FixedUpdate()
+    {
+        if(!TrackPlayer)
+            return;
+        if(Vector3.Distance(this.transform.position + CenterOffset, Networking.LocalPlayer.GetPosition()) < MaxDetectionDistance)
+            RotatableTransform.rotation = Quaternion.LookRotation((Networking.LocalPlayer.GetBonePosition(HumanBodyBones.Head) - (RotatableTransform.transform.position + CenterOffset)).normalized, Vector3.up) * Quaternion.Euler(AdjustmentRotation);
+        else
+            RotatableTransform.rotation = ZeroRotation;
+    }
+}
