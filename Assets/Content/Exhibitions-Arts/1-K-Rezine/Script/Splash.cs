160831696f1aa9356789430a936a1d8f5f8c3fdb
iptnkt <125504831+iptnkt@users.noreply.github.com>
2023-08-16 22:14:17 +0300

    KC-17-Shprots-Woman-reworkKapkan

---
+++ Assets/Content/Exhibitions-Arts/1-K-Rezine/Script/Splash.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Splash : UdonSharpBehaviour
+{
+    public GameObject Target;
+    public float splashPos;
+    public ParticleSystem splashParticle;
+
+    void FixedUpdate()
+    {
+        float rot = Target.transform.position.y;
+        if(-rot< splashPos)
+        {
+            splashParticle.Play();
+
+        }
+        else
+        {
+            splashParticle.Stop();
+        }
+    }
+}
