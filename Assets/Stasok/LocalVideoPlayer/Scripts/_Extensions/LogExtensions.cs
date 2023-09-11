a4981f5b18692b3a4f58b7fe2af6923bdc6e73af
kobyakova <42976161+kobyakova@users.noreply.github.com>
2023-09-11 17:46:35 +0300

    Added translator + video-player

---
+++ Assets/Stasok/LocalVideoPlayer/Scripts/_Extensions/LogExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Logger
+{
+    public static void Log(string methodName, string description)
+    {
+        Debug.Log($"<{methodName}> [{description}]\n");
+    }
+}
