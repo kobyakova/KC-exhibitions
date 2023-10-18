a045ad2bd2019112f73bff630d8b1831e9d5b397
kobyakova <42976161+kobyakova@users.noreply.github.com>
2023-10-18 14:19:11 +0300

    Added sound

---
+++ Assets/Content/Sound/Scripts/playsoundbyevent.cs
@@ -0,0 +1,19 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class playsoundbyevent : UdonSharpBehaviour
+{
+   
+public AudioSource aud;
+
+
+   
+    public void play_sound()
+    {
+        aud.Play();
+    }
+}
+
